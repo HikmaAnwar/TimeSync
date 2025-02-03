03a6ef1225898121623dc3baf1edbfc84ece3629
Hikma Anwar <114664132+HikmaAnwar@users.noreply.github.com>
2025-02-03 21:30:18 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hey bopti bpti!");
+  res.send("Hey bopti!");
 });
 
 app.listen(5000, () => {
